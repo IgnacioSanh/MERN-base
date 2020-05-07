417f7e668c9892f4bc9dfec97cf48e29dd939c79
Ignacio Sanhueza <ignacio.sanhueza.l@gmail.com>
2020-05-07 18:02:08 -0400

    The private information is handled by the keys.js file

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const config = require("./config/key");
+const { User } = require("./models/user");
 const app = express();
 
 mongoose
-  .connect("mongodb://localhost:27017/newDB", { useNewUrlParser: true })
+  .connect(config.mongoURI, { useNewUrlParser: true })
   .then(() => console.log("Connected to mongodb"))
   .catch((err) => console.log(err));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
   res.send("Hello world");
+});
+
+app.post("/api/user/register", (req, res) => {
+  const user = new User(req.body);
+  console.log(user);
+  user.save((err, userData) => {
+    if (err) return res.json({ success: false, err });
+  });
+  return res.status(200).json(user);
 });
 
 console.log("Listening on port 5000");
